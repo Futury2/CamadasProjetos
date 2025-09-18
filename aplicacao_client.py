4ece79afa0a5ab06402a319f6696eda24b271cf2
karyne <karyneam@al.insper.edu.br>
2025-09-18 15:26:56 -0300

    projeto4

--- aplicacao_client.py
+++ aplicacao_client.py
@@ -3,15 +3,43 @@ import sys
 import os, time
 from enlace import enlace
 from protocol import *
+from datetime import datetime
 
 # ajuste a porta do CLIENTE
-serialName = "COM9"  # troque pela sua
+serialName = "COM9"  
 
+LOG_FILE = "client_log.txt"
 DOWNLOAD_DIR = "./client_downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 TIMEOUT = 5.0
 
+def log_event(sent_recv, h, payload=b"", error_msg=None):
+    """
+    sent_recv: 'envio' ou 'receb'
+    h: dicionário do cabeçalho (parse_header)
+    payload: bytes do payload
+    """
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        ts = datetime.now().strftime("%d/%m/%Y %H:%M:%S.%f")[:-3]
+        if error_msg:
+            tipo = "ERRO"
+            tam = "-"
+            linha = f"{ts} / {sent_recv} / {tipo} / {tam} / {error_msg}"
+        else:
+            tipo = h["type"]
+            tam = h["payload_len"] + HEADER_SIZE + len(EOP)
+        
+            linha = f"{ts} / {sent_recv} / {tipo} / {tam}"
+
+            # Se for DATA, incluir extras
+            if tipo == T_DATA:
+                crc = checksum16(payload)
+                linha += f" / {h['seq']} / {h['total_pkts']} / {crc:04X}"
+        
+        f.write(linha + "\n")
+
+#espera até chegar uma quantidade no buffer, se passo um tempo e não chegou, retorna None
 def wait_bytes_with_timeout(com, needed, timeout):
     start = time.time()
     while time.time() - start < timeout:
@@ -20,6 +48,7 @@ def wait_bytes_with_timeout(com, needed, timeout):
         time.sleep(0.02)
     return None
 
+#tenta ler o header primeiro, depois le o corpo, verifica integridade, se tiver ok retorna e se falhar retorna None
 def recv_packet_with_timeout(com, timeout=TIMEOUT):
     hdr = wait_bytes_with_timeout(com, HEADER_SIZE, timeout)
     if hdr is None: 
@@ -32,13 +61,22 @@ def recv_packet_with_timeout(com, timeout=TIMEOUT):
     payload = bytes(rest[:-len(EOP)])
     eop = bytes(rest[-len(EOP):])
     ok, msg, _ = verify_packet(hdr, payload, eop)
+    if ok:
+        log_event("receb", h, payload)
     if not ok:
+        log_event("receb", error_msg=msg)
         return None, msg
     return (h, payload), "ok"
 
+#envia um pacote e espera os pacotes do servidor, só retorna quando chega um pacote
 def send_and_wait(com, pkt, expect_types, timeout=TIMEOUT):
     com.sendData(pkt)
+    hdr = parse_header(pkt[:HEADER_SIZE])
+    payload = pkt[HEADER_SIZE:-len(EOP)] if len(pkt) > HEADER_SIZE + len(EOP) else b""
+    log_event("envio", hdr, payload)
+    start = time.time()
     while True:
+        remaining = max(0.1, timeout - (time.time() - start))
         resp, why = recv_packet_with_timeout(com, timeout)
         if not resp:
             return None, why
@@ -55,6 +93,12 @@ def print_progress_bar(filename, received, total, length=40):
         if received == total:
             print()
 def main():
+
+    #ERRO CRC###############################################
+    erro_crc_proposital = True 
+    crc_enviado = False
+    #######################################
+
     print("-----------------------------------------------------------------------------------")
     print("┹┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄ ▭ ▬▬▬▬▬▬▟ ✩ ▙▬▬▬▬▬▬ ▭ ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┲")
     print("┹┄┄┄┄┄┄┄┄┄┄┄┄┄┄ ▭ Tranferência de Arquivos entre Client e Servidor ▭ ┄┄┄┄┄┄┄┄┄┄┄┄┄┲")
@@ -70,14 +114,14 @@ def main():
     print("Byte de sacrifício enviado.")
 
     try:
-        # 1) HELLO -> FILE_LIST
+        #handshake inicial, cliente envia hello, servidor responde com a lista de arquivos disponíveis
         (h, pl), _ = send_and_wait(com, build_packet(T_HELLO), expect_types=[T_FILELIST], timeout=10)
         files = pl.decode().split("|") if pl else []
         print("\nArquivos disponíveis no servidor:")
         for i, name in enumerate(files, 1):
             print(f"  {i}. {name}")
 
-        # 2) seleção de >=2 arquivos
+        #seleção dos arquivos
         wanted = []
         while True:
             sel = input("Digite o nome do arquivo a adicionar (ou ENTER para finalizar): ").strip()
@@ -91,7 +135,7 @@ def main():
             print("Selecione pelo menos 2 arquivos.")
             return
 
-        # manda N x FILE_REQ e guarda FILE_IDs
+        #requisição dos arquivos 
         id_to_name = {}
         for idx, name in enumerate(wanted, start=1):
             (hh, ppl), _ = send_and_wait(
@@ -102,7 +146,7 @@ def main():
             id_to_name[fid] = name
             print(f"Servidor confirmou {name} com FILE_ID={fid} (total_pkts={hh['total_pkts']})")
 
-        # sinaliza START
+        #transferência dos arquivos, cliente manda start e servidor confirma que vai começar com end no início da mensagem
         resp, why = send_and_wait(com, build_packet(T_START), expect_types=[T_END], timeout=10)
         if not resp:
             print("CLIENTE: falha ao iniciar download:", why)
@@ -115,10 +159,12 @@ def main():
         time.sleep(0.5)
 
 
-        # 3) recebimento: grava por FILE_ID, envia ACK por pacote
+        # cria buffers para armazenar os bytes de cada arquivo, para cada pacote DATA, extrai o id, acrescenta o payload, atualiza a barra, responde com ACK, se flag_last estiver ativa, arquivo acabpou
         buffers = {fid: bytearray() for fid in id_to_name}
         pkts_count = {fid: 0 for fid in id_to_name}
         finished = set()
+
+        last_sent = {}  # guarda último pacote enviado (ACK ou DATA)
 
         print("\nBaixando... (P=pausa, R=resume, Q=abort)")
         print("-----------------------------------------------------------------------------------")
@@ -146,6 +192,11 @@ def main():
                     break
         threading.Thread(target=key_thread, daemon=True).start()
 
+
+        #ERRO DE PACOTE 
+        erro_proposital = True  # ativa ou desativa erro proposital
+        erro_enviado = False    # controla se o erro já ocorreu
+
         while len(finished) < len(buffers) and not ctrl["aborted"]:
             pkt, why = recv_packet_with_timeout(com, timeout=10)
             if not pkt:
@@ -154,13 +205,37 @@ def main():
             h, payload = pkt
             if h["type"] == T_DATA:
                 fid, seq = h["file_id"], h["seq"]
+
+                # === ERRO PROPOSITAL PULAR PACOTE ==============
+                if erro_proposital and not erro_enviado:
+                    # Exemplo: pular o pacote 2
+                    if seq == 2:
+                        print(f"Erro proposital: pacote {seq}")
+                        log_event("receb", h, payload)  # loga tentativa
+                        erro_enviado = True
+                        continue  # pacote não é processado nem ACK enviado
+                # =============================
+
+                # ERRO CRC por exemplo seq == 3 ##############################################
+                if erro_crc_proposital and not crc_enviado and seq == 3:
+                    pkt = build_packet(T_DATA, file_id=fid, seq=seq, payload=payload)
+                    pkt_corrompido = pkt[:10] + b"\x00\x00" + pkt[12:]  # checksum errado
+                    com.sendData(pkt_corrompido)
+                    log_event("envio", parse_header(pkt_corrompido[:HEADER_SIZE]), pkt_corrompido[HEADER_SIZE:-len(EOP)])
+                    print(f"Erro proposital: pacote seq {seq} enviado com CRC errado")
+                    crc_enviado = True
+                    continue  # não processa payload nem envia ACK
+                #############################################################################
+                
                 # (cheque duplicados, se quiser, pelo seq)
                 buffers[fid].extend(payload)
                 pkts_count[fid] += 1
                 # Atualiza barra de progresso
                 print_progress_bar(id_to_name[fid], pkts_count[fid], h["total_pkts"])
                 # ACK
-                com.sendData(build_packet(T_ACK, file_id=fid, seq=seq))
+                ack_pkt = build_packet(T_ACK, file_id=fid, seq=seq, payload=b"OK")
+                com.sendData(ack_pkt)
+                last_sent[(fid, seq)] = ack_pkt
                 if (h["flags"] & FLAG_LAST) != 0:
                     finished.add(fid)
                     print(f"CLIENTE: arquivo {id_to_name[fid]} finalizado ({pkts_count[fid]} pacotes)")
@@ -171,8 +246,16 @@ def main():
                 print("Servidor:", payload.decode(errors="ignore"))
                 if payload != b"done":
                     break
+                    ...
+            elif h["type"] == T_ACK:
+                if payload == b"CRC_ERROR":
+                    fid, seq = h["file_id"], h["seq"]
+                    print(f"CLIENTE: servidor pediu retransmissão -> file_id={fid}, seq={seq}")
+                if (fid, seq) in last_sent:
+                    com.sendData(last_sent[(fid, seq)])
+                    print(f"CLIENTE: pacote {seq} reenviado")
 
-        # 4) salvar arquivos e resumo
+        #salva arquivo 
         print("-----------------------------------------------------------------------------------")
         print("\nResumo:")
         total_bytes = 0
--- protocol.py
+++ protocol.py
@@ -1,5 +1,22 @@
 # protocol.py
 import struct
+import crcmod
+
+#3 partes fixas
+#Header - 12 bytes
+#payload - 100 bytes - parte real do arqui sendo transferido 
+#EOP - 4 bites - indica que o pacote terinou, 4bytes fixos 
+#Total = 116 bytes
+
+#HEADER
+# tipo do pacotes 1
+# id do arquivo 1
+# numero de sequencia do pacotes 2
+# numero total de pacotes do arquivo 2
+# quantos bytes de payload esse pacote carrega 2
+# verificação de integridade(ckecksun) 2
+# flag_last para marcar o ultimo pacote 2
+
 
 EOP = b"\xAA\xBB\xCC\xDD" #indica o final do pacote
 HEADER_SIZE = 12 #o cabeçalho tem 12 bytes
@@ -8,31 +25,31 @@ MAX_PAYLOAD = 100 #tamanho máximo dos dados que podem ser enviados em pacote
 # TIPOS
 #permite que o receptor saiba a finalidade do pacote
 #representa o tipo de mensagem
-T_HELLO    = 0
-T_FILELIST = 1
-T_FILEREQ  = 2
-T_FILEOK   = 3
-T_START    = 4
-T_DATA     = 5
-T_ACK      = 6
-T_PAUSE    = 7
+#HANDSHAKE, sequencis de bytes iniciais entre client e servidor antes da transmissão dos arquivos
+T_HELLO    = 0 #cliente  pergunta se o servidor ta ativo
+T_FILELIST = 1 #servidor resonde com a lista de arquivo
+T_FILEREQ  = 2 #cliente pede o arquivo, manda o nome no payload
+T_FILEOK   = 3 #servidor confirma e envia o id e o total de pacotes
+T_START    = 4 #cliente sinaliza que terminou a escolha e está pronto para dowload
+T_DATA     = 5 #intercala os pacotes, envia um pacote do arquivo A, outro do B
+T_ACK      = 6 #cliente responde cada pacote com T_ACK, confirmação, recebi o pacote x fo arquivo y corretamente
+T_PAUSE    = 7 
 T_RESUME   = 8
 T_ABORT    = 9
-T_END      = 10
+T_END      = 10 #servidor responde que vai iniciar a transmisão
 
 FLAG_LAST = 1  #ultimo pacote de uma sequência
 
-#detecta erros de transmissão, soma todos os bytes do pacote e se algum foi alterado durante a transmissão,a soma final não irá corresponder ao valor inicial
+_crc16 = crcmod.mkCrcFun(0x11021, initCrc=0xFFFF, rev=False, xorOut=0x0000)
 def checksum16(data: bytes) -> int:
-    return sum(data) & 0xFFFF
+    return _crc16(data) & 0xFFFF
 
 #cria o cabeçalho do pacote
 #recebe todas as informações necessárias e empacota em um cabeçalho
 def pack_header(tp, file_id, seq, total_pkts, payload_len, flags, payload):
     # checksum é calculado com campo checksum=0
     # ceda letra represeta um campo do cabeçalho
-    hdr_wo_ck = struct.pack(">BBHHHHH", tp, file_id, seq, total_pkts, payload_len, 0, flags)
-    cks = checksum16(hdr_wo_ck + payload) #check sum é calculado antes, por isso ele é 0 temporariamente, pois ele não deve fazer parte do cálculo
+    cks = checksum16(payload) #check sum é calculado antes, por isso ele é 0 temporariamente, pois ele não deve fazer parte do cálculo
     return struct.pack(">BBHHHHH", tp, file_id, seq, total_pkts, payload_len, cks, flags)
 
 #monstar o pacote completo, recebe o tipo do pacote e os dados(payload) e chama a função pck_header para criar o cabeçalho, concatena o cabeçalho e dados e o EOP
@@ -58,8 +75,7 @@ def verify_packet(hdr: bytes, payload: bytes, eop: bytes):
     if eop != EOP: #checa a sequência final de bytes, se corresponde ao EOP
         return False, "bad EOP", h
     # recompute checksum with checksum field=0
-    hdr_wo_ck = hdr[:8] + b"\x00\x00" + hdr[10:12] #recalcula o checksun do pacote e compara com o valor que veio no cabeçalho original
-    cks = checksum16(hdr_wo_ck + payload)
+    cks = checksum16(payload)
     if cks != h["checksum"]:
         return False, "bad checksum", h
     return True, "ok", h
